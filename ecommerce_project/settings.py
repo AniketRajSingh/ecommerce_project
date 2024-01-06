bd96c9249b32f1216e17c61744267ef033411b40
Aniket Raj Singh <mohitrajingh223@gmail.com>
2024-01-06 21:39:22 +0530

    update

--- ecommerce_project/settings.py
+++ ecommerce_project/settings.py
@@ -84,25 +84,26 @@ WSGI_APPLICATION = 'ecommerce_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 # load_dotenv()
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': os.getenv('DB_ENGINE', 'django.db.backends.sqlite3'),
-#         'NAME': os.getenv('DB_NAME', BASE_DIR / "db.sqlite3"),
-#         'USER': os.getenv('DB_USER', ''),
-#         'PASSWORD': os.getenv('DB_PASSWORD', ''),
-#         'HOST': os.getenv('DB_HOST', ''),
-#         'PORT': os.getenv('DB_PORT', ''),
-#     }
-# }
+DATABASES = {
+  'default': {
+    'ENGINE': 'django.db.backends.postgresql',
+    'NAME': 'DummyDB',
+    'USER': 'mohitrajingh223',
+    'PASSWORD': 'IeQlmH0CMj6v',
+    'HOST': 'ep-white-smoke-13219721.ap-southeast-1.aws.neon.tech',
+    'PORT': '5432',
+    'OPTIONS': {'sslmode': 'require'},
+  }
+}
 
 #RazorPay Integration 
 RAZORPAY_KEY_ID = 'rzp_test_esc8vkRRLTMjAN'
